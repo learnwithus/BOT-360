22dea548bc978ef2e147d94cdac7a87c4bea5ab2
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-02-06 15:05:16 -0800

    add php templates for BOT to repo

---
+++ vgh-360.php
@@ -0,0 +1,34 @@
+<?php
+
+/* Template Name: VGH-360 */
+
+?>
+
+<?php
+  get_header();
+?>
+
+<iframe id="iframe" src="http://vchdesign.ca/BOT-360/vgh.html"></iframe>
+
+<script type="text/javascript">
+  function resizeIframe() {
+    var headerHeight = document.getElementById('main-header').clientHeight;
+    var htmlMargin = window.getComputedStyle(document.documentElement).marginTop.replace('px', '');
+  	var totalHeight = +headerHeight + +htmlMargin;
+  	var heightString = "height: calc(100vh - " + totalHeight + "px)";
+    console.log(headerHeight + " " + htmlMargin +" "+ totalHeight)
+    document.getElementById('iframe').setAttribute("style", heightString);
+  }
+  window.onresize = function(event) {
+    resizeIframe();
+  };
+  window.onload = function(event) {
+    resizeIframe();
+  }
+</script>
+
+<?php
+  for ($i = 1; $i < 18; $i++) {
+      echo '<link rel="prefetch" href="http://vchdesign.ca/BOT-360/assets/vgh/vgh' . $i . '.jpg">';
+  }
+?>
